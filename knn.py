e247d2bf0824129f9a5e2c1d5c5448866583cd46
Nishanth Prem <nishanthprem8@gmail.com>
2024-06-13 17:28:13 -0700

    K-NearestNeighbhors classifier.

--- knn.py
+++ knn.py
@@ -1,22 +1,22 @@
-#%% Importing the libraries
+#%% Importing Libraries
 
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 
-#%% Loading and identifying the dependent and independent variables
+#%% Loading and splitting the dataset
 
 df = pd.read_csv('Social_Network_Ads.csv')
 X = df.iloc[:, :-1].values
 y = df.iloc[:, -1].values
 
-#%% Splitting the data into train and test set
+#%% Splitting the data
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=0)
+    X, y , test_size=0.25, random_state=0)
 
 #%% Feature Scaling
 
@@ -24,29 +24,19 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-#%% Applying Logistic Regression
+#%% Initializing the classifier
 
-classifier = LogisticRegression(random_state=0)
+classifier = KNeighborsClassifier()
 classifier.fit(X_train, y_train)
-
 
 #%% Prediction
 
-prediction = classifier.predict(sc.transform([[30,87000]]))
-
-#%% Predicting X_test
-
 y_pred = classifier.predict(X_test)
 
-#%% Confusion Matrix for accuracy
+#%% Confusion Matrix and accuracy score
 
 cm = confusion_matrix(y_test, y_pred)
-accuracy = accuracy_score(y_test, y_pred)
 print(cm)
+accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
-
-#%% Visualization
-
-
-
 
