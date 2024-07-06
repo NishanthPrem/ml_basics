e295c2df23ebfbb68bc3d3cb476a774e6e5190e9
Nishanth Prem <nishanthprem8@gmail.com>
2024-07-06 12:20:49 -0700

    Random Forest Classifier

---
+++ random_forest_classifier.py
@@ -0,0 +1,38 @@
+#%% Importing the libraries
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+#%% Importing the dataset
+
+df = pd.read_csv('Social_Network_Ads.csv')
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+#%% Splitting the dataset
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+#%% Feature Scaling
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#%% Decision Tree
+
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
+
+#%% Prediction
+
+y_pred = classifier.predict(X_test)
+
+#%% Evaluating the model
+
+cm = confusion_matrix(y_test, y_pred)
+acc = accuracy_score(y_test, y_pred)
